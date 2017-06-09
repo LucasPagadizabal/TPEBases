ae7646d4d348f62bb741c541c9f505ce1b131206
Pedro di Fonzo <pedrodifonzo@gmail.com>
2017-06-09 16:32:32 -0300

    alta competencia

--- controller/Competencia.php
+++ controller/Competencia.php
@@ -22,6 +22,8 @@ class Competencia{
 
   function altaComp(){
     $comp = $_POST;
+    $this->modelCompetencia->addCompetencia($comp);
+    $this->formAltaComp();
     //mandar al model para guardar
   }
 
